f1658682f26f2669ebdcc8fb2d5393b1232ce76a
Phyo Yarzar <phyoyazar592@gmail.com>
2023-07-02 01:25:50 +0630

    WIP in user UI

--- src/feature/auth/Login.tsx
+++ src/feature/auth/Login.tsx
@@ -1,28 +1,27 @@
-import { Box, Button, PasswordInput, Stack, TextInput } from "@mantine/core";
-import { useRouter } from "next/router";
-import React from "react";
+import { Box, Button, PasswordInput, Stack, TextInput } from '@mantine/core';
+import { useRouter } from 'next/router';
 
 export const Login = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <Box
-      w={300}
-      p={24}
-      sx={(theme) => ({
-        boxShadow: theme.shadows.md,
-        borderRadius: theme.radius.md,
-        border: `1px solid ${theme.colors.gray[3]}`,
-      })}
-    >
-      <Stack>
-        <TextInput placeholder="Your email" label="Email" />
-        <PasswordInput placeholder="Password" label="Password" />
+	return (
+		<Box
+			w={300}
+			p={24}
+			sx={(theme) => ({
+				boxShadow: theme.shadows.md,
+				borderRadius: theme.radius.md,
+				border: `1px solid ${theme.colors.gray[3]}`,
+			})}
+		>
+			<Stack spacing={20}>
+				<TextInput placeholder='Your email' label='Email' />
+				<PasswordInput placeholder='Password' label='Password' />
 
-        <Button color="orange" onClick={() => void router.push("/")}>
-          Login
-        </Button>
-      </Stack>
-    </Box>
-  );
+				<Button color='orange' onClick={() => void router.push('/')}>
+					Login
+				</Button>
+			</Stack>
+		</Box>
+	);
 };
