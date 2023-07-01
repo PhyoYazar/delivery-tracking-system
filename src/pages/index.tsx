320c49a20972c66a21423e1ae27119243d540e7c
Phyo Yarzar <phyoyazar592@gmail.com>
2023-07-01 23:55:11 +0630

    setup done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,22 @@
-import { Box, Center, Text, Title } from "@mantine/core";
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
-import { Login } from "~/feature/auth/Login";
-import { api } from "~/utils/api";
+import { Box, Center, Title } from '@mantine/core';
+import Head from 'next/head';
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+	// const hello = api.example.hello.useQuery({ text: 'from tRPC' });
 
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-        <meta name="description" content="Delivery Tracking System" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+				<meta name='description' content='Delivery Tracking System' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-      <Box w="100vw" h="100vh">
-        <Center h="100%">
-          <Title>This is Home page</Title>
-        </Center>
-      </Box>
-    </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
+			<Box w='100vw' h='100vh'>
+				<Center h='100%'>
+					<Title>This is Home page</Title>
+				</Center>
+			</Box>
+		</>
+	);
 }
